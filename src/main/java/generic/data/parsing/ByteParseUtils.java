332c4dfffa4d335ca2d6e209d9f9db07b5a4fe2d
arttusalminen <arttu.mtb@gmail.com>
2024-07-19 23:21:48 +0300

    Added & clarified parsing methods

--- src/main/java/generic/data/parsing/ByteParseUtils.java
+++ src/main/java/generic/data/parsing/ByteParseUtils.java
@@ -14,27 +14,55 @@ public class ByteParseUtils {
         return extractedBytes;
     }
 
-    public static float getFloat(byte[] b, int start, int end) {
-        return ByteBuffer.wrap(extractBytes(b, start, end))
+    public static float getFloat32(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 4))
                 .order(ByteOrder.LITTLE_ENDIAN)
                 .getFloat();
     }
 
-    public static long getLong(byte[] b, int start, int end) {
-        return ByteBuffer.wrap(extractBytes(b, start, end))
+    public static long getLong64(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 8))
                 .order(ByteOrder.LITTLE_ENDIAN)
                 .getLong();
     }
 
-    public static int getInt(byte[] b, int start, int end) {
-        return ByteBuffer.wrap(extractBytes(b, start, end))
+    public static byte getByte8(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 1))
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .get();
+    }
+
+    public static int getShort16(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 2))
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .getShort();
+    }
+
+    public static int getInt32(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 4))
                 .order(ByteOrder.LITTLE_ENDIAN)
                 .getInt();
     }
 
-    public static double getDouble(byte[] b, int start, int end) {
-        return ByteBuffer.wrap(extractBytes(b, start, end))
+    public static double getDouble64(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 8))
                 .order(ByteOrder.LITTLE_ENDIAN)
                 .getDouble();
+    }
+
+    public static char getChar16(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 2))
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .getChar();
+    }
+
+    public static boolean getBoolean(byte[] b, int start) {
+        return ByteBuffer.wrap(extractBytes(b, start, start + 1))
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .get() != 0;
+    }
+
+    public static String getString(byte[] b, int start, int charCount) {
+        return new String(extractBytes(b, start, start + charCount));
     }
 }
